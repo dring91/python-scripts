dd19ef68a90ac79b2a2a47a7bd64ef036bddd413
David Ring <david.j.ring@outlook.com>
2016-03-23 16:05:08 -0400

    undo_bondswap working

--- undo_bondswap.py
+++ undo_bondswap.py
@@ -3,6 +3,7 @@ from sys import argv, exit
 import os
 from getopt import *
 from conf_tools import write_conf, write_xyz
+from copy import copy
 
 def PBC(x,boundaries):
   
@@ -25,7 +26,7 @@ def GetAtoms(file):
       section = L[0]
     elif len(L) > 3 and (L[3] == 'xhi' or L[3] == 'yhi' or L[3] == 'zhi'):
       box.append(L[:2])
-    elif len(L) > 2 and L[2] == '1' and section == 'Atoms':
+    elif len(L) > 2 and L[2] in ['1','2'] and section == 'Atoms':
       atoms.update({L[0]:L[1:6]})
     elif len(L) > 1 and L[1] == '1' and section == 'Bonds':
       bonds.update({L[2]:L[3]})
@@ -66,18 +67,16 @@ def unshuffleAtoms(atoms, bonds, ends, nMon):
 
   unshuffled = []
   # unshuffle all the 'forwards' chains and then flip the 'backwards' ones and unshuffle the remaining
-  for tail in ends[0:5]:
-    if tail in unshuffled:
-      continue
-    if tail in bonds.keys:
+  for tail in ends:
+    if tail in bonds.keys():
       atom = tail
       unshuffled.append(atoms[atom]) 
-      for n in range(nMon):
+      for n in range(nMon-1):
         atom = bonds.pop(atom)
         unshuffled.append(atoms.pop(atom)) 
-      print len(unshuffled)
-    elif tail in bonds.values:
-      print 'Value in dict reversed'
+    elif tail in bonds.values():
+      # print 'Value in dict reversed'
+      continue
 
   return unshuffled
 
@@ -90,15 +89,17 @@ def main():
 
   # search for all the chain ends in bonds and store in new array
   ends = getEnds(atoms, bonds)
-  print ends
 
-  # # unshuffle bonds
-  # unshuffled = unshuffleAtoms(atoms, bonds, ends, nMon)
+  # unshuffle bonds
+  unshuffled = unshuffleAtoms(copy(atoms), copy(bonds), ends, nMon)
+  atoms = [[str(i+1)]+atom for i,atom in enumerate(unshuffled)] + \
+          [[key]+atom for key,atom in atoms.iteritems() if atom[1] == '2']
+  bonds = [[str(i+1),'1',key,val] for i,(key,val) in enumerate(bonds.iteritems())]
 
-  # # write the new atoms list and the bonds to a file
-  # suffix = '_unshuffled'
-  # write_conf(inFile+suffix, atoms, bonds, 'unshuffled conf', [2,1], box, [1,1])
-  # write_xyz(inFile+suffix, atoms)
+  # write the new atoms list and the bonds to a file
+  suffix = '_unshuffled'
+  write_conf(inFile+suffix, atoms, bonds, 'unshuffled conf', [2,1], box, [1,1])
+  write_xyz(inFile+suffix, [atom[2:] for atom in atoms])
 
   print 'Finished Analyzing Trajectory'
       
