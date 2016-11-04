6437cfc0f461d93a3cd2338f92600dd43eada6bd
David Ring <david.j.ring@outlook.com>
2016-11-04 11:52:46 -0400

    deleted old files

--- C_CPP/compile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-name=$1
-gcc -std=c11 -g -Wall ${name}.c -o ${name}.out
--- test.py
+++
@@ -1,5 +0,0 @@
-import conf_tools
-
-with open('../trajectories/N_25_traj_eps055_1.lammpstrj','r') as file:
-  time, box, atoms = conf_tools.readTrj(file)
-  print time, box, atoms.shape
