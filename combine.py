461dcc42ad207e6fe87d2d9fabf85dfc92efe464
David Ring <david.j.ring@outlook.com>
2016-11-08 12:07:21 -0500

    combine working for cylinders and surfaces

--- combine.py
+++ combine.py
@@ -25,16 +25,21 @@ def getArgs(argv):
   return prefixFile, suffixFile, outFile
 
 def formatOutput(atoms, bonds, types):
-  # No modification (should make the formatting choices clearer)
-  # Needs a way to renumber molecules 
-  top = [atom[:2] + [str(int(atom[2]) + 0)] + atom[3:] for atom in atoms[:types[0]]]
-  # Increments atom number and molecule number
+  # Separates top part of trajectory
+  #top = [atom[:2] + [str(int(atom[2]) + 0)] + atom[3:] for atom in atoms[:types[0]]]
+  top = [atom for atom in atoms[:types[0]]]
+  # Separates bottom part of trajectory
+  #           str(                                                      )
+  #               int(       )+int(          )-int(                  )+1
+  #                   atom[0]      top[-1][0]      atoms[        ][0]
+  #                                                      types[0]
   bottom  = [[str(int(atom[0])+int(top[-1][0])-int(atoms[types[0]][0])+1),
-               str(int(atom[1])+int(top[-1][1])-int(atoms[types[0]][1])+1)
-              ] + atom[2:] for atom in atoms[types[0]:]]
-  bonds = [bond[:2] + [str(int(bond[2])+int(top[-1][0])-int(atoms[types[0]][0])+1),
-                       str(int(bond[3])+int(top[-1][0])-int(atoms[types[0]][0])+1)]
-                       for bond in bonds] 
+              str(int(atom[1])+int(top[-1][1])-int(atoms[types[0]][1])+1)
+             ] + atom[2:] for atom in atoms[types[0]:]]
+  # # fixes bonds
+  # bonds = [bond[:2] + [str(int(bond[2])+int(top[-1][0])-int(atoms[types[0]][0])+1),
+  #                      str(int(bond[3])+int(top[-1][0])-int(atoms[types[0]][0])+1)]
+  #                      for bond in bonds] 
 
   return top + bottom, bonds
 
@@ -87,43 +92,48 @@ def main():
   with open(inFile2, 'r') as inp2:
     box2, atoms2, bonds = readConf(inp2,atomSet)
 
-  # unwrap atoms
-  lengths = [float(ax[1])-float(ax[0]) for ax in box1]
-  # assumes that there are periodic image indices 
-  coords2 = [[float(xyz[3+i]) + float(xyz[6+i])*lengths[i] for i in range(3)] for xyz in atoms2]
-  atoms2 = [atoms2[i][:3] + [str(xyz) for xyz in coords2[i]] for i in range(len(coords2))]
+  # # calculates box side lengths
+  # lengths = [float(ax[1])-float(ax[0]) for ax in box1]
+  # # unwrap atomic coordinates (assumes that there are periodic image indices)
+  # coords2 = [[float(xyz[3+i]) + float(xyz[6+i])*lengths[i] for i in range(3)] for xyz in atoms2]
+  # # combines atomic information with unwrapped coordinates
+  # atoms2 = [atoms2[i][:3] + [str(xyz) for xyz in coords2[i]] for i in range(len(coords2))]
 
-  # sort atoms
+  # change atom id number to type int
   atoms1 = [[int(row[0])] + row[1:6] for row in atoms1] 
   atoms2 = [[int(row[0])] + row[1:6] for row in atoms2] 
 
+  # sort atoms
   atoms1.sort()
   atoms2.sort()
 
+  # change atom id number back to type str
   atoms1 = [[str(row[0])] + row[1:] for row in atoms1] 
   atoms2 = [[str(row[0])] + row[1:] for row in atoms2] 
 
   # find the vertical bounds of the two configurations
-  topBounds = boundAtoms(atoms1)
-  bottomBounds = boundAtoms(atoms2)
+  bounds1 = boundAtoms(atoms1)
+  bounds2 = boundAtoms(atoms2)
 
-  # shift the bottom atoms to the appropriate location
-  buffer = -1
-  shift = topBounds[0]-bottomBounds[1]+buffer
+  # shift the bottom atoms to the appropriate location and leave a small gap between confs
+  margin = -1
+  shift = bounds1[1]-bounds2[0]+margin
   atoms2 = translateAtoms(atoms2, [0,0,shift]) 
 
-  # join and format the configurations
+  # join and format the configuration information
   atoms = atoms1 + atoms2
   types = [len(atoms1),len(atoms2)]
   atoms, bonds = formatOutput(atoms, bonds, types)
 
   # construct the box dimensions
-  box = box1[:2] + [[str(bottomBounds[0]+shift),str(topBounds[1])]]
-  title = 'saturated infiltration simulation with cylindrical capillary' 
-  ntypes = 3
+  box = box1[:2] + [[str(bounds2[0]+shift),str(bounds1[1])]]
+  # title = 'saturated infiltration simulation with cylindrical capillary' 
+  title = 'cylindrical capillary + flat base' 
+  # ntypes = 3
+  ntypes = 1
 
   write_xyz(outFile, [line[2:] for line in atoms])
-  write_conf(outFile, atoms, bonds, title, [ntypes,1], box, [1] * ntypes)
+  write_conf(outFile, atoms, bonds, title, [ntypes,0], box, [1] * ntypes)
 
   # # check that the bonds and atoms correspond properly
   # test_atoms = np.array(atoms)
