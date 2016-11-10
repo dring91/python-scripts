f55c46d7a0e9b6d3f5375c9388fc4311d0e80db5
David Ring <david.j.ring@outlook.com>
2016-11-10 15:09:28 -0500

    improved select_frames.py

--- select_frames.py
+++ select_frames.py
@@ -1,41 +1,25 @@
-#!/usr/bin/python
-
-import sys
+from sys import argv,exit
 from conf_tools import *
 from pbc_tools import *
+import argparse
 
 MODE = 'r'
-X = 0
-Y = 1
-Z = 2
 
 def main():
-  try:
-    inFile = sys.argv[1]
-  except IndexError:
-    print "No Filename given"
-    sys.exit()
 
-  nFrames = 20
-  nTotal = 200
-  nSkip = int(nTotal/nFrames)
-  cont = 'yes'
+  parser = argparse.ArgumentParser()
+  parser.add_argument("-i", "--input", required=True)
+  parser.add_argument("-o", "--output", required=True)
+  parser.add_argument("-t", "--step")
+  args = parser.parse_args()
 
-  with open(inFile+'.xyz', MODE) as inp:
-    try:
-      outFile = sys.argv[2]
-    except IndexError:
-#       cont = raw_input('Warning: output filename is the same as the input file. This may overwrite data. Continue? [y]/n: ')
-#       if cont == 'no' or cont == 'n':
-#         sys.exit()
-#       else:
-      outFile = inFile
+  suffix = '_sparse'
 
-    suffix = '_sparse'
+  with open(args.input+'.xyz', MODE) as inp:
     for n in range(nFrames):
-      for k in range(nSkip):
-        time, frame = readFrame(inp)
-      write_xyz(outFile+suffix, time, frame, 'a')
+      time, frame = readFrame(inp)
+      if time % args.step == 0:
+        write_xyz(args.output+suffix, frame, time, 'a')
 
 if __name__ == '__main__':
   main()
