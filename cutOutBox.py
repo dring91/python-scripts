89a6b5825556d754c24cdd1d13441eb0c7ba35da
David Ring <david.j.ring@outlook.com>
2016-11-11 09:50:09 -0500

    modified cutOutBox to include substrate

--- cutOutBox.py
+++ cutOutBox.py
@@ -6,60 +6,48 @@ from conf_tools import *
 from regions import inBox, inRange
 import argparse
 
-# def cut(atoms, chainLength, box):
-#   n = 0
-#   molecule = []
-#   drop = []
-#   newBox = [[-0.5*R+comp,0.5*R+comp] for comp in origin]
-#   # loop over each molecule
-#   for atom in atoms:
-#     if n == chainLength:
-#       molecule = np.array(molecule)
-#       # unnecessary because of unwrap
-#       #molecule = unwrap(molecule, box)
-#       # calculate its center of mass
-#       COM = [calcCOM(molecule[:,d]) for d in range(len(molecule[0]))]
-#       # check whether it's in the region
-#       if inBox(COM, newBox): # if inRange(COM[0],[-0.5*R,0.5*R]): # inCylinder(COM, R, 0.5*R, origin):
-#         # add it to drop if it is
-#         drop.extend(molecule)
-#       n = 0
-#       molecule = []
-#     n += 1
-#     molecule.append(atom)
-# 
-#   return drop
-
 def filter_atoms(atoms, atomtype):
-
-  # select atoms of type atomtype
-  atoms = atoms[atoms[:,2] == atomtype]
 
   # sort atoms
   atoms = atoms[atoms[:,0].argsort()]
   atoms[:,:2] = atoms[:,:2] + 1 - atoms[0,:2]
 
-  return atoms
+  # select atoms of type atomtype
+  polymer = atoms[atoms[:,2] == atomtype]
+  substrate = atoms[atoms[:,2] != atomtype]
+
+  return polymer, substrate
 
 def cut(atoms, chainLength, atomtype, box):
   
-  # calculate COM for each molecule
-  nMol = len(atoms)/chainLength
-  atoms = atoms.reshape((nMol, chainLength, 6))
-  com = atoms[:,:,3:].mean(1)
+  # separate atoms by type
+  polymer, substrate = filter_atoms(atoms, atomtype)
+  
+  # filter substrate atoms
+  interior = ((substrate[:,3:5] > box[:2,0]) & (substrate[:,3:5] < box[:2,1]))
+  interior = (interior.sum(1) == 2)
+  substrate = substrate[interior]
+
+  # calculate COM for each polymer molecule
+  nMol = len(polymer)/chainLength
+  polymer = polymer.reshape((nMol, chainLength, 6))
+  com = polymer[:,:,3:].mean(1)
 
   # check that each COM is in the box
   interior = ((com > box[:,0]) & (com < box[:,1]))
   interior = (interior.sum(1) == 3)
 
-  # collect all molecules with COM inside box
-  atoms = atoms[interior]
+  # collect all polymer molecules with COM inside box
+  polymer = polymer[interior]
 
   # make the bonds
-  bonds = generate_bonds(atoms, atomtype)
+  bonds = generate_bonds(polymer, atomtype)
 
   # reshape the atoms array so that it has only 2 axes
-  atoms = atoms.reshape((-1,6))
+  polymer = polymer.reshape((-1,6))
+
+  # join the two atom arrays along their outer axis
+  atoms = np.concatenate((polymer,substrate), axis=0)
 
   return atoms, bonds
 
@@ -100,13 +88,15 @@ def main():
 
   # read in configuration file
   with open(args.input, 'r') as inp:
-    box, atoms, bonds = readConf(inp,['1'])
+    box, atoms, bonds = readConf(inp,['1','2'])
 
   atoms = np.array(atoms,dtype=float)
   box = np.array(box,dtype=float)
   bonds = np.array(bonds,dtype=float)
 
-  atoms = filter_atoms(atoms, atomtype)
+  # set the vertical zero
+  atoms[:,5] -= box[2,0]
+  print box[2,0]
 
   # reconstruct box using the new dimensions provided
   if args.x is not None:
