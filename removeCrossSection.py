20c124738dc8720435bfd9f2a4c15d8789b39113
David Ring <david.j.ring@outlook.com>
2017-01-04 15:11:06 -0500

    fixed removeCrossSection.py

--- removeCrossSection.py
+++ removeCrossSection.py
@@ -8,6 +8,49 @@ NDIMS = 3
 AREA = 100*100
 RC = 0
 
+def calc_porosity(particles, polymers, box, ns, params):
+  # calculate bin size
+  particles = particles.reshape((ns['nPart'],ns['nAtoms'],ns['nDim']))
+  dSlice = (box[2,1] - box[2,0]) / ns['nBins']
+
+  # initialize porosity
+  porosity = np.zeros((ns['nBins'],3))
+  porosity[:,0] = (np.arange(ns['nBins']) + 0.5) * dSlice + box[2,0]
+
+  # unwrap particle coordinates
+  particles = unwrap(particles, box)
+
+  # find the transforms to translate and rotate particles
+  rotations, centers = calcTransformations(particles, box)
+  
+  # Select insertion points
+  points = np.random.rand(ns['nInsert'],3)
+  points = points*(box[:,1] - box[:,0]) + box[:,0]
+  
+  # test for overlap with nanoparticles
+  test = np.zeros_like(points[:,0],dtype=bool)
+  for c,r in zip(centers, rotations):
+    test = insert(points, c, r, box, test, params)
+
+  # calculate the superficial polymer density
+  volume = dSlice * (box[0,1] - box[0,0]) * (box[1,1] - box[1,0])
+  bead = np.pi/6
+  bins = ((polymers - box[2,0]) / dSlice).astype(int)
+  superficial = np.array([(bins == bin).sum()*bead/volume for bin in range(ns['nBins'])])
+
+  # partition points by vertical height
+  bins = ((points[:,2] - box[2,0]) / dSlice).astype(int)
+  #porosity[:,1] = np.zeros(ns['nBins'])
+
+  # calculate the ratio of accepted to total insertions
+  for bin in range(ns['nBins']):
+    insertions = np.logical_not(test[bins == bin])
+    porosity[bin,1] = insertions.sum() / float(len(insertions))
+
+  porosity[:,2] = porosity[:,1] - superficial
+
+  return porosity
+
 def getArgs(argv):
   
   parser = argparse.ArgumentParser()
@@ -76,7 +119,9 @@ def main():
   outFile = '%s_I%d_B%d' % (args.output, args.nInsert, args.nSlices)
 
   # params = (a,b,r_cut,nAtomsPerPart,nPart)
-  params = (25*0.5, 50*0.5, RC, 4684, 54)
+  #params = (25*0.5, 50*0.5, RC, 4684, 54)
+  params = {'a':25*0.5, 'b':50*0.5, 'r':1}
+  ns = {'nPart':54, 'nAtoms':4684, 'nDim':3, 'nBins':args.nSlices, 'nInsert':args.nInsert}
 
   with open('density_'+outFile, 'w') as otp:
     otp.write('# Density profiles\n')
@@ -98,34 +143,35 @@ def main():
       # polymers = frame[frame[:,0] == 1][:,3]
       # particles = frame[frame[:,0] >= 3][:,1:]
 
-      pmin = polymers.min()
-      polymers -= pmin
-      particles[:,2] -= pmin
+      #pmin = polymers.min()
+      #polymers -= pmin
+      #particles[:,2] -= pmin
 
       # make box
       box = np.zeros((NDIMS,2))
-      box[:,0] = particles.min(0)
-      box[:,1] = particles.max(0)
+      box[:,0] = particles.min(0)-0.5
+      box[:,1] = particles.max(0)+0.5
 
       # Calculate the packing porosity
-      porosity = calcPorosity(particles, box, args.nInsert, args.nSlices, params)
+      porosity = calc_porosity(particles, polymers, box, ns, params)
+      #porosity = calcPorosity(particles, box, args.nInsert, args.nSlices, params)
       # calculate the superficial density of the polymers in the packing
       density = makeHistogram(porosity, polymers, args.nSlices, box[2])
 
-      # Integrate real density to the appropriate cut-off
-      cutOff = 0.85
-      height85 = integrateHeight(density, cutOff)
+      # # Integrate real density to the appropriate cut-off
+      # cutOff = 0.85
+      # height85 = integrateHeight(density, cutOff)
 
-      # Integrate real density to the appropriate cut-off
-      cutOff = 0.99
-      height99 = integrateHeight(density, cutOff)
+      # # Integrate real density to the appropriate cut-off
+      # cutOff = 0.99
+      # height99 = integrateHeight(density, cutOff)
 
       # output density and height values
       with open('density_'+outFile, 'a') as otp:
         header = '# time: %d\n#  z  density  porosity' % time
         np.savetxt(otp, density, fmt='%.5f', header=header, footer='\n', comments='')
-      with open('height_'+outFile, 'a') as otp:
-        np.savetxt(otp, [[time, height85, height99, density[1,0]]], fmt='%d %.5f %.5f %.5f',comments='')
+#      with open('height_'+outFile, 'a') as otp:
+#        np.savetxt(otp, [[time, height85, height99, density[1,0]]], fmt='%d %.5f %.5f %.5f',comments='')
       
   print 'Finished analyzing trajectory'
 
