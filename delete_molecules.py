e193297f659e00350a2b512a31dc02511d6487f8
David Ring <david.j.ring@outlook.com>
2017-03-06 12:59:02 -0500

    removed slag; commented code

--- delete_molecules.py
+++ delete_molecules.py
@@ -9,46 +9,44 @@ def main():
   parser.add_argument('-i','--input')
   parser.add_argument('-o','--output')
   parser.add_argument('-n','--nMon', type=int)
+  parser.add_argument('-s','--saturation', type=float, 
+                      help='specify the saturation level; \
+                            that is, tell the algorithm what fraction of molecules \
+                            to remove from the configuration')
   args = parser.parse_args()
   
   with open(args.input) as file:
-    box, atoms, bonds = readConf(file, ['1','2','3'])
-    DOS = 0.8
+    box, atoms, bonds = readConf(file)
 
-    # generate a few very large bins (larger than the RE of the polymer)
-    # count the number of molecules in each bin by computing which bin has the most beads from each chain
-    # delete a given fraction of molecules from a given bin
-    
-    atoms = atoms[np.argsort(atoms[:,0])]
-    polymers = atoms[atoms[:,2] == 1] #[:,5]
-    polymers = polymers.reshape((len(polymers)/args.nMon, args.nMon, atoms.shape[-1]))
-    atoms = atoms[atoms[:,2] != 1]
-    print polymers.shape
-    
-    # nBins = 15
-    # bin_size = (box[2,1] - box[2,0]) / nBins
+  # degree of (under-)saturation (used to calculate the number of molecules to remove)
+  DOS = 0.8
+  DOS = args.saturation
+ 
+  # sort atoms according to atomic ID
+  atoms = atoms[np.argsort(atoms[:,0])]
+  # extract only polymers (type == 1)
+  polymers = atoms[atoms[:,2] == 1]
+  # reshape polymers so that each subarray contains exactly one molecule
+  polymers = polymers.reshape((len(polymers)/args.nMon, args.nMon, atoms.shape[-1]))
+  # erase polymers from atoms array
+  atoms = atoms[atoms[:,2] != 1]
+  print polymers.shape
+  
+  # calculate the number of molecules to keep in the polymers array
+  nMolecules = int((1 - DOS) * polymers.shape[0])
+  # slice polymers array and keep nMolecules
+  polymers = polymers[:nMolecules]
+  print polymers.shape
 
-    # in_bin = (polymers - box[2,0])
+  # reshape polymers
+  polymers = polymers.reshape((-1, atoms.shape[-1]))
 
-    # try other methods first:
-    # method 1: remove final molecules
-    # method 2: remove every few molecules
-    # method 3: remove at random
+  # write modified polymers into atom matrix
+  atoms = np.concatenate((polymers, atoms), axis=0)
 
-    # method 1
-    nMolecules = int((1 - DOS) * polymers.shape[0])
-    polymers = polymers[:nMolecules]
-    print polymers.shape
-
-    # reshape polymers
-    polymers = polymers.reshape((-1, atoms.shape[-1]))
-
-    # write modified polymers into atom matrix
-    atoms = np.concatenate((polymers, atoms), axis=0)
-
-    write_conf(args.output, atoms.astype('|S16'), bonds, box, {"atoms":3, "bonds":1}, [1]*3, '% undersaturated trajectory')
-    write_traj(args.output, np.delete(atoms[:,:6],1,1), box)
-    
+  write_conf(args.output, atoms.astype('|S16'), bonds, box, {"atoms":2, "bonds":1}, [1]*2, '% undersaturated trajectory')
+  write_traj(args.output, np.delete(atoms[:,:6],1,1), box)
+  
 
 if __name__ == '__main__':
   main()
