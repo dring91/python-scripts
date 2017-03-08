5648a6d0451a3984bb7517cf9595a6bfd6fcdb7f
David Ring <david.j.ring@outlook.com>
2017-03-08 13:16:57 -0500

    delete_molecules now capable of defining a region in which to keep molecules

--- delete_molecules.py
+++ delete_molecules.py
@@ -8,18 +8,20 @@ def main():
   parser = argparse.ArgumentParser()
   parser.add_argument('-i','--input')
   parser.add_argument('-o','--output')
-  parser.add_argument('-n','--nMon', type=int)
+  parser.add_argument('-l','--length', type=int)
   parser.add_argument('-s','--saturation', type=float, 
                       help='specify the saturation level; \
                             that is, tell the algorithm what fraction of molecules \
                             to remove from the configuration')
+  parser.add_argument('method', choices={'random','region'},
+                      help='choose the method for removing molecules')
   args = parser.parse_args()
   
   with open(args.input) as file:
     box, atoms, bonds = readConf(file)
 
   # degree of (under-)saturation (used to calculate the number of molecules to remove)
-  DOS = 0.8
+  #DOS = 0.8
   DOS = args.saturation
  
   # sort atoms according to atomic ID
@@ -27,7 +29,7 @@ def main():
   # extract only polymers (type == 1)
   polymers = atoms[atoms[:,2] == 1]
   # reshape polymers so that each subarray contains exactly one molecule
-  polymers = polymers.reshape((len(polymers)/args.nMon, args.nMon, atoms.shape[-1]))
+  polymers = polymers.reshape((len(polymers)/args.length, args.length, atoms.shape[-1]))
   # erase polymers from atoms array
   atoms = atoms[atoms[:,2] != 1]
   print polymers.shape
@@ -35,8 +37,16 @@ def main():
   # calculate the number of molecules to keep in the polymers array
   nMolecules = int((1 - DOS) * polymers.shape[0])
   # slice polymers array and keep nMolecules
-  polymers = polymers[:nMolecules]
-  print polymers.shape
+  if args.method == 'random':
+    polymers = polymers[:nMolecules]
+    print polymers.shape
+  elif args.method == 'region':
+    cx = box[:2,1] - box[:2,0]
+    area = cx[0] * cx[1]
+    height = nMolecules * args.length / area
+    com = polymers[:,:,5].mean(1)
+    polymers = polymers[com < height]
+    print polymers.shape
 
   # reshape polymers
   polymers = polymers.reshape((-1, atoms.shape[-1]))
