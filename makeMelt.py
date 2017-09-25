b994bca322354f73a04b31dadff1e7ee79253254
David Ring <david.j.ring@outlook.com>
2017-09-25 11:16:54 -0400

    makeMelt.py doesn't generate randomly oriented polymers

--- makeMelt.py
+++ makeMelt.py
@@ -21,6 +21,49 @@ def rotations(v, angle, axis, inv=False):
 
   return v.dot(R.T)
 
+def make_box(boundary, limits, chains, density, total, radius=None, volume=None):
+
+  box = np.zeros((3,2))
+  # Generate random starting points for chains
+  start_pts = np.random.rand(chains, 3)
+  volume = total / density
+
+  if boundary == 'box':
+    l = (volume)**(1/3)
+    box = np.full_like(box,l/2)
+    box[:,0] *= -1
+    print(box)
+    
+    start_pts = np.random.rand(chains, 3)
+    start_pts = (start_pts - 0.5) * (box[:,1] - box[:,0])
+  
+  if boundary == 'plane':
+    # make box
+    box[:2] = np.array(limits).reshape((2,2))
+    h = volume / np.diff(box[:2],axis=1).prod()
+    box[2,1] = h
+    print(box)
+
+    start_pts[:,:2],start_pts[:,2] = (start_pts[:,:2] - 0.5) * (box[:2,1] - box[:2,0]),\
+                                     start_pts[:,2] * h
+    #info = np.stack((np.arange(args.nChains)+1,np.ones(args.nChains)),axis=1)
+    #write_traj('start-pts', np.concatenate((info,start_pts),axis=1), box, mode='w')
+
+  if boundary == 'cylinder':
+    # make box
+    h = volume / (np.pi * radius**2)
+    box[2,1] = h
+    box[:2,1] = radius+0.5
+    box[:2,0] = -radius-0.5
+
+    # scale starting points to r, theta, z values
+    start_pts *= [radius-0.5, 2*np.pi, h]
+    # convert starting point values to x and y
+    start_pts[:,0], start_pts[:,1] = start_pts[:,0]*np.cos(start_pts[:,1]), \
+                                     start_pts[:,0]*np.sin(start_pts[:,1])
+
+  return box, start_pts
+
 def main():
 
   """ Algorithm
@@ -37,7 +80,7 @@ def main():
   parser.add_argument("-l","--limits",nargs='+',type=float,help="Box limits")
   parser.add_argument("-r","--radius",type=float,help="Radius of cylindrical melt")
   parser.add_argument("--reflect",default=[],nargs='+',choices={'x','y','z'})
-  parser.add_argument("-b", "--boundary", choices=['plane','cylinder'])
+  parser.add_argument("-b", "--boundary", choices=['plane','cylinder','box'])
   parser.add_argument("-d","--density",type=float)
   args = parser.parse_args()
 
@@ -47,44 +90,14 @@ def main():
   dims = {'x':0,'y':1,'z':2}
 
   # approximate melt density and volume
-  volume = total / args.density
-  
-  if args.boundary == 'plane':
-    # make box
-    box = np.zeros((3,2))
-    box[:2] = np.array(args.limits).reshape((2,2))
-    h = volume / np.diff(box[:2],axis=1).prod()
-    box[2,1] = h
-    print(box)
-
-    # Generate random starting points for chains
-    start_pts = np.random.rand(args.nChains, 3)
-    start_pts[:,:2],start_pts[:,2] = (start_pts[:,:2] - 0.5) * (box[:2,1] - box[:2,0]),\
-                                     start_pts[:,2] * h
-    #info = np.stack((np.arange(args.nChains)+1,np.ones(args.nChains)),axis=1)
-    #write_traj('start-pts', np.concatenate((info,start_pts),axis=1), box, mode='w')
-
-  if args.boundary == 'cylinder':
-    # make box
-    box = np.zeros((3,2))
-    h = volume / (np.pi * args.radius**2)
-    box[2,1] = h
-    box[:2,1] = args.radius+0.5
-    box[:2,0] = -args.radius-0.5
-
-    # Generate random starting points for chains
-    start_pts = np.random.rand(args.nChains, 3)
-    # scale starting points to r, theta, z values
-    start_pts *= [args.radius-0.5, 2*np.pi, h]
-    # convert starting point values to x and y
-    start_pts[:,0], start_pts[:,1] = start_pts[:,0]*np.cos(start_pts[:,1]), \
-                                     start_pts[:,0]*np.sin(start_pts[:,1])
+  box, start_pts = make_box(args.boundary, args.limits, args.nChains, args.density, total)
 
   # randomly generate the bond angles of the polymer
   nAngles = args.nMon - 1
   angles = np.random.rand(args.nChains, nAngles, 2)
   bond_min = np.arccos((2*bond_length**2-1.02**2)/2*bond_length**2)
-  angles[:,:,0] = angles[:,:,0] * (np.pi - bond_min) + bond_min
+  angles[:,0,0] = angles[:,0,0] * 2 * np.pi
+  angles[:,1:,0] = angles[:,1:,0] * (np.pi - bond_min) + bond_min
   angles[:,:,1] = angles[:,:,1] * 2 * np.pi
 
   # initialize array of cartesian coordinates
@@ -101,8 +114,8 @@ def main():
   # accumulate xyz coordinates to form a walk
   coords = np.cumsum(coords, axis=1)
 
-  # add starting points
-  coords = np.swapaxes(coords,0,1)
+  ## add starting points
+  roords = np.swapaxes(coords,0,1)
   coords = coords + start_pts
   coords = np.swapaxes(coords,0,1)
 
@@ -139,7 +152,9 @@ def main():
   # massage data into output file
   atoms = np.zeros((total,9))
   atoms[:,0] = np.arange(total)+1
-  atoms[:,1] = np.repeat(np.arange(args.nChains)+1,args.nMon)
+  #atoms[:,1] = np.repeat(np.arange(args.nChains)+1,args.nMon)
+  chain = list(range(args.nMon//2)) + list(range(args.nMon - args.nMon//2)[::-1])
+  atoms[:,1] = np.tile(chain,args.nChains)
   atoms[:,2] = types
   atoms[:,3:6] = coords
 
